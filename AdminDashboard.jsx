d55b6d2df73242a18d05710c60caf6cafbb62fbe
Eric <mateieric66@gmail.com>
2025-05-02 14:54:27 +0300

    Update and rename Admindashboard.jsx to AdminDashboard.jsx

---
+++ AdminDashboard.jsx
@@ -0,0 +1,12 @@
+export default function AdminDashboard() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold">Admin Panel</h1>
+      <ul className="mt-4">
+        <li>Users</li>
+        <li>Vehicles</li>
+        <li>Transactions</li>
+      </ul>
+    </div>
+  );
+}
